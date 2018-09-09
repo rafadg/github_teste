2dda520a066f35c2346e41cb0aa10c781cc2e1cd
Rafael Dantas <rafaeld.g@outlook.com>
2018-09-09 19:24:08 -0300

    Atulizacao Java V0.1

---
+++ Principal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Principal{
+	public static void main(String[] args){
+		Scanner tec = new Scanner(System.in);
+		int n = 1 + new Random().nextInt(20);
+		int chute = 0;
+		int tentativas = 0;
+		boolean naoEncontrou = true;
+		System.out.println("--Iniciando jogo--");	
+		while(naoEncontrou){
+			tentativas++;
+			System.out.println("Chute um numero: ");
+			chute = tec.nextInt();
+			if(chute == n){
+				naoEncontrou = false;
+			}else if(chute > n){
+				System.out.println("Chute um numero menor");
+			}else{
+				System.out.println("Chute um numero maior");
+			}
+		}
+		
+		System.out.println("Você ganhou com " + tentativas + " tentativas");		
+	}
+}
